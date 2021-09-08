599547bf4b06f057befdf1ef2a3b28d280e05968
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-09-08 18:52:04 +0530

    [Ariprasath] Add: printing the harmonic progression till the limit given by the user

---
+++ HarmonicNumber.py
@@ -0,0 +1,16 @@
+'''
+@Author: Ariprasath
+@Date: 2021-09-08 18:43:00
+@Last Modified by: Ariprasath
+@Last Modified time: 2021-09-08 18:36:00
+@Title : Harmonic number
+'''
+
+limit=int(input("Enter the limit\n"))
+initial_value=1
+while initial_value<=limit:
+    if initial_value<limit:
+        print(f"1/{initial_value}+",end='')
+    elif initial_value==limit:
+        print(f"1/{initial_value}",end='')
+    initial_value+=1
